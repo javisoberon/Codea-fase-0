31b6d549c2970ad0efcebbae1ef315361871a873
codeamac01 <codeamac01@gmail.com>
2017-01-16 18:56:50 -0600

    pig latin

---
+++ javi_pig_latin.rb
@@ -0,0 +1,47 @@
+def pig_latin_word
+  p "Write a word"
+  word = gets.chomp
+  emptystring = ""
+  word.downcase!
+  i = 0
+  until word[i].match(/[aeiou]/)
+    i += 1
+  end
+
+  if i == 0
+    p emptystring = word + "way"
+  else
+    p emptystring = word.slice(i,word.length-i) + word.slice(0, i) + "ay"
+  end
+end
+
+def pig_latin_sentence
+  p "write a sentence"
+  sentence = gets.chomp
+  emptystring = ""
+  array = []
+  answer = ""
+  array = sentence.split(" ")
+  array.each do |word|
+    word.downcase!
+    i = 0
+    until word[i].match(/[aeiou]/)
+      i += 1
+    end
+
+    if i == 0
+      emptystring = word + "way" + " "
+      answer << emptystring
+    else
+      emptystring = word.slice(i,word.length-i) + word.slice(0, i) + "ay" + " "
+      answer << emptystring
+    end
+  end
+  answer.rstrip!
+  p answer
+
+end
+#Pruebas
+
+pig_latin_word
+pig_latin_sentence
